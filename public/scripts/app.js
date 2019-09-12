704057e4462e09775a0bbe48ccdc558b10e5a254
berkererol <berkererol@gmail.com>
2019-09-12 15:12:37 +0000

    Implemented jquery and ajax to request new tweets.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,105 +4,101 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Heidegger",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@zeitundsein"
-    },
-    "content": {
-      "text": "Language is the house of the truth of Being."
-    },
-    "created_at": 9261127859088
-  },
-  {
-    "user": {
-      "name": "Bhagavad Gita",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@vyasa"
-    },
-    "content": {
-      "text": "You are what you believe in. You become that which you believe you can become."
-    },
-    "created_at": 92611220399087
-  },
-];
-
-const createTweetElement = function (tweet) {
-  const $tweet = $("<article>")
-    .addClass("tweetpost")
-    .append(
-      $("<header>")
-        .addClass("tweetpostheader")
-        .append(
-          $("<div>")
-            .addClass("tweetpostheader-bar-left")
-            .append(
-              $("<img>")
-                .addClass("post-avatar")
-                .attr("src", tweet.user.avatars),
-              $("<p>")
-                .addClass("user-info")
-                .append(
-                  $("<span>")
-                    .addClass("user-name")
-                    .text(tweet.user.name),
-                  $("<span>")
-                    .addClass("user-account")
-                    .text(tweet.user.handle)
-                ),
-            ),
-          $("<div>")
-            .addClass("tweetpostheader-bar-right")
-            .append(
-              $("<p>")
-                .addClass("tweet-date")
-                .text(tweet.created_at)
-            ),
-        ),
-      $("<p>")
-        .addClass("tweetpost")
-        .text(tweet.content.text)
-    );
-  return $tweet;
-};
-
-
-const renderTweets = function (tweets) {
-
-  tweets.forEach(item => {
-    let renderTweet = createTweetElement(item);
-    $("#tweet-container").prepend(renderTweet);
-  });
-
-};
-
+//DOCUMENT READY ----------------------------------------------
 $(document).ready((event) => {
 
-  renderTweets(data);
+  // Accepts tweet object returns the formatted HTML to be appended onto the tweets-container
+  const createTweetElement = function (tweet) {
+    const $tweet = $("<article>")
+      .addClass("tweetpost")
+      .append(
+        $("<header>")
+          .addClass("tweetpostheader")
+          .append(
+            $("<div>")
+              .addClass("tweetpostheader-bar-left")
+              .append(
+                $("<img>")
+                  .addClass("post-avatar")
+                  .attr("src", tweet.user.avatars),
+                $("<p>")
+                  .addClass("user-info")
+                  .append(
+                    $("<span>")
+                      .addClass("user-name")
+                      .text(tweet.user.name),
+                    $("<span>")
+                      .addClass("user-account")
+                      .text(tweet.user.handle)
+                  ),
+              ),
+            $("<div>")
+              .addClass("tweetpostheader-bar-right")
+              .append(
+                $("<p>")
+                  .addClass("tweet-date")
+                  .text(tweet.created_at)
+              ),
+          ),
+        $("<p>")
+          .addClass("tweetpost")
+          .html(tweet.content.text)
+      );
+    return $tweet;
+  };
 
+
+  const renderTweets = function (tweets) {
+    //first empty container to load new tweets
+    // $("#tweet-container").empty();
+    tweets.forEach(item => {
+      let renderTweet = createTweetElement(item);
+      $("#tweet-container").prepend(renderTweet);
+    });
+
+  };
+
+  // loadTweets() renders the tweets if the GET request was successful
+  const loadTweets = function () {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      success: function (response) {
+        renderTweets(response);
+      }
+    });
+  };
+
+  //Validate the appropriate length of the tweet
+  const validateTweetLength = function (data) {
+    if (data.val().length === 0) {
+      alert("Tweet is empty!")
+      return false;
+    } else if (data.val().length > 140) {
+      alert("Your tweet is over 140 character limit.")
+      return false;
+    }
+    return true;
+  };
+
+
+  $("#tweet-form").on("submit", function (event) {
+    event.preventDefault();
+    const data = $(this).serialize();
+
+    if (!validateTweetLength($("#tweet-text-area"))) {
+      // $("#error").css("display", "inherit");
+    } else if (validateTweetLength($("#tweet-text-area"))) {
+      $.ajax('/tweets', {
+        method: 'POST',
+        data: data
+      }).done(function (newTweet) {
+        $("#tweet-text-area").val("");
+        $(".counter").text(140);
+        // $("#error").css("display", "none");
+        loadTweets();
+      })
+    };
+  });
 });
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -12,16 +12,3 @@ $(document).ready(function() {
   });
 });
 
-// $(document).ready(function() {
-//   $("#tweet-area")
-//     .keyup(function() {
-//       let value = $(this).val().length;
-//       $("#counter").text(140 - value);
-//       if (140 - value < 0) {
-//         $("#counter").css("color", "red");
-//       } else {
-//         $("#counter").css("color", "black");
-//       }
-//     })
-//     .keyup();
-// });
